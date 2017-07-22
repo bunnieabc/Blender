7611ebae40b5b8b64c3dd92d8403a435ed154ecc
bunnieabc <bunnieabc@gmail.com>
2017-07-23 03:33:47 +0800

    first function done

--- resources/gradient_color.js
+++ resources/gradient_color.js
@@ -27,18 +27,6 @@ var getColorNum = function(num1, num2, index) {
   return color_num
 }
 
-// function inputBlock() {
-//   let alert = buildDialog('Create Color Scale', 'How many steps do you want in the scale?')
-
-//   alert.addTextLabelWithValue('Steps')
-//   alert.addTextFieldWithValue('5')
-//   alert.addButtonWithTitle('OK')
-//   alert.addButtonWithTitle('Cancel')
-
-//   return alert;
-// }
-
-
 ////// Normal Text Info
 // var app = [NSApplication sharedApplication];
 // [app displayDialog:"This is an alert box!" withTitle:"Alert Box Title"];
@@ -55,7 +43,12 @@ if (selectedCount == 0) {
 
   var first_layer_color = getColor("fill", selectedLayers[0])
   var last_layer_color = getColor("fill", selectedLayers[selectedCount-1])
+  var first_layer_pos_x = selectedLayers[0].rect().origin.x
+  var last_layer_pos_x = selectedLayers[selectedCount-1].rect().origin.x
+  var first_layer_pos_y = selectedLayers[0].rect().origin.y
+  var last_layer_pos_y = selectedLayers[selectedCount-1].rect().origin.y
 
+  log("test" + first_layer_pos_x + " " + last_layer_pos_x)
   copied_layers.push(selectedLayers[0])
   copied_layers.push(selectedLayers[1])
 
@@ -64,34 +57,33 @@ if (selectedCount == 0) {
     //var layer = selectedLayers[0].duplicate();
     var new_layer = layer_rec.duplicate()
     new_layer.select_byExpandingSelection(true, true);
-    //copied_layers.push(layer_rec)
+  
     copied_layers.splice(k+1, 0, new_layer )
     layer_rec = new_layer
-    //log(copied_layers.count())
-    //log(k)
-    //layer.addToSelection()
-    //selectedLayers = context.selection;
-    //log(selectedLayers.count())
   }
-  
 
-
-  
   selectedCount = copied_layers.length;
 
-  log("1")
-
+  
   for (var i = 0; i < selectedCount; i++) {
     var layer = copied_layers[i];
+    
+    // color 
     var selectedColor = getColor("fill", layer)
     var r, g, b;
-    log("2")
+
     var r = Math.round(getColorNum(first_layer_color.red(), last_layer_color.red(), i ) * 255)
     var g = Math.round(getColorNum(first_layer_color.green(), last_layer_color.green(), i ) * 255)
     var b = Math.round(getColorNum(first_layer_color.blue(), last_layer_color.blue(), i ) * 255)
     //var red = Math.round(selectedColor.red() * 255)
     var fill = layer.style().fills().firstObject();
     fill.color = MSColor.colorWithRed_green_blue_alpha( r / 255, g / 255, b / 255, 1.0);
-    //log(red)
+    
+    // position 
+    var rect = layer.rect()
+    var x = Math.round(getColorNum(first_layer_pos_x, last_layer_pos_x, i ))
+    var y = Math.round(getColorNum(first_layer_pos_y, last_layer_pos_y, i ))
+    layer.rect = NSMakeRect(x, y, rect.size.width, rect.size.height);
+    //log(Math.round(getColorNum(first_layer_pos, last_layer_pos, i )))
   }
 };
