813f689457e42a037f4d5f0c1536abc14c7dce4b
bunnieabc <bunnieabc@gmail.com>
2017-08-02 18:06:29 +0800

    change folder name

--- Sketch/test.js
+++
@@ -1,3 +0,0 @@
-var doc = context.document;
-var result = [doc askForUserInput:"How many copies do you want?" initialValue:"10"];
-var result2 = [doc askForUserInput:"How many copies do you want?" initialValue:"10"];
